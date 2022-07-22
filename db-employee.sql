1c933fa3c4ae0226840c2aa992d699473e6dabc9
devwsilveirasouza <dev.wsilveirasouza@gmail.com>
2022-07-21 22:04:54 -0300

    DataTables ServerSide With Ajax Pagination

---
+++ db-employee.sql
@@ -0,0 +1,7 @@
+INSERT INTO `employee`.`employees` (`username`, `name`, `email`) VALUES ('maria.julia', 'Julia Maria', 'j.mary.p@example.com');
+INSERT INTO `employee`.`employees` (`username`, `name`, `email`) VALUES ('julia.paulo', 'julia Paulo', 'julia.p@example.com');
+INSERT INTO `employee`.`employees` (`username`, `name`, `email`) VALUES ('ana.julia', 'ana julia', 'ana.j@example.com');
+INSERT INTO `employee`.`employees` (`username`, `name`, `email`) VALUES ('paulo.lucas', 'paulo lucas', 'paulo.l@example.com');
+INSERT INTO `employee`.`employees` (`username`, `name`, `email`) VALUES ('lucas.paulo', 'lucas maria', 'lucas.m@example.com');
+INSERT INTO `employee`.`employees` (`username`, `name`, `email`) VALUES ('nair.belo', 'Nair Belo', 'belo.n@example.com');
+INSERT INTO `employee`.`employees` (`username`, `name`, `email`) VALUES ('ivo.rocha', 'Ivo Rocha', 'ivo.r@example.com');
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[EmployeesController::class, 'index']);
+Route::get('/employees/getEmployees/',[EmployeesController::class,'getEmployees'])->name('employees.getEmployees');
